cefd6ee32bf39bdd769ce43cc7ce640ed08f2717
Nox-dev25 <umang.cantech@gmail.com>
2025-12-10 19:18:25 +0530

    Create page.tsx

---
+++ app/paas-pricing/page.tsx
@@ -0,0 +1,12 @@
+import "./paas-pricing.css";
+import Navbar from "../components/Navbar";
+import { PricingFilter } from "./components/PricingFilter";
+
+export default function PassPricing() {
+  return (
+  <main >
+    <Navbar />
+    <PricingFilter />
+</main>
+  );
+}
